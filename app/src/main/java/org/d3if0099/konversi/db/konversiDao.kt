fa17090bd615128babd34fcb4b84e0f2d996ab5b
Lutfan Davi <lutfan04@gmail.com>
2023-05-22 09:49:59 +0700

    membuat database dengan room

---
+++ app/src/main/java/org/d3if0099/konversi/db/konversiDao.kt
@@ -0,0 +1,14 @@
+package org.d3if0099.konversi.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+
+interface KonversiDao {@Insert
+fun insert(konversi: KonversiEntity)
+    @Query("SELECT * FROM konversi ORDER BY id DESC LIMIT 1")
+    fun getLastKonversi(): LiveData<KonversiEntity?>
+}
